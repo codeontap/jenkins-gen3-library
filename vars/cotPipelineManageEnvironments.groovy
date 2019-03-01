d09fc3159f493412b78cb7f7461f188910d6b3c3
roleyfoley <roleyfoley@gmail.com>
2019-03-01 16:39:53 +1100

    update properties lookup

--- vars/cotPipelineManageEnvironments.groovy
+++ vars/cotPipelineManageEnvironments.groovy
@@ -1,33 +1,94 @@
 #!/usr/bin/env groovy
 
+@NonCPS
+def createBooleanParameter(String desc, String value){
+
+   return [$class: 'BooleanParameterDefinition', defaultValue: true, description: desc, name: value]
+}
+
 def call( String propertiesFile, String agentLabel ) {
 
-    def productProperties 
-    pipeline { 
-        agent { 
-            label "${agentLabel}"
-        }
-        stages {
-            stage('ManageEnvironment') {
-                script {
-                    productProperties = readProperties interpolate: true, file: propertiesFile;
-                }
-                input {
-                    message 'Please Provide Parameters'
-                    ok 'Start'
-                    parameters {
-                        choice(name: 'DEPLOYMENT_MODE', choices: "${propertiesFile["DEPLOYMENT_MODE_LIST"].split(',').join('\n')}", description: 'Select the deployment mode')
-                        choice(name: 'ENVIRONMENT', choices: "${propertiesFile["ENVIRONMENT_LIST"].split(',').join('\n')}", description: 'Select the Environment to manage')
-                        choice(name: 'SEGMENT', choices: "${propertiesFile["SEGMENT_LIST"].split(',').join('\n')}", description: 'Select the Segment to manage')
-                        string(name: 'COMMENT', defaultValue: '', description: 'Added to the git commit message' )
-                        booleanParam( name: 'TREAT_RUN_ID_DIFFERENCES_AS_SIGNIFICANT', defaultValue: false, description: 'Set this to force redeployment where only the runid value has changed. Mainly used where data is in S3.')
-                    }
-                }
-                steps {
-                    sh 'env'
-                    manageEnvironment propertiesFile
-                }
-            }
-        }
+    def defaultProperties = [DEPLOYMENT_MODE_LIST: 'update,stop', LEVELS_LIST: 'segment,solution,application', GENERATION_DEBUG: '', AUTOMATION_DEBUG: '' ]
+    productProperties = readProperties interpolate: true, file: propertiesFile, defaults: defaultProperties;
+
+    def basicParameters = input message: 'Please Provide Parameters', ok: 'Start', parameters: [
+            choice(name: 'DEPLOYMENT_MODE', choices: "${productProperties["DEPLOYMENT_MODE_LIST"].split(",").join("\n")}", description: 'Desired way in which deploy should occur' ),
+            choice(name: 'ENVIRONMENT', choices: "${productProperties["ENVIRONMENT_LIST"].split(",").join("\n")}", description: 'Environment to manage'),
+            choice(name: 'SEGMENT', choices: "${productProperties["SEGMENT_LIST"].split(",").join("\n")}", description: 'Segment to manage' ),
+            string(name: 'COMMENT', defaultValue: '', description: 'Added to the git commit message' ),
+            booleanParam( name: 'TREAT_RUN_ID_DIFFERENCES_AS_SIGNIFICANT', defaultValue: false, description: 'Set this to force redeployment where only the runid value has changed. Mainly used where data is in S3.')
+        ]
+
+    // Levels
+    def levelParameters = []
+    productProperties["LEVELS_LIST"].split(",").each { 
+        levelParameters += createBooleanParameter('Level', it )
     }
+    def levelInputs = input(
+
+        id: 'userInput', message: 'Component Levels', parameters: levelParameters
+    )
+    def levels = []
+    levelInputs?.findAll{ it.value }?.each {
+        levels += [ it.key.toString() ]
+    }
+    levels = levels.join(",")
+    
+    // Segment units
+    def segmentUnitParameters = []
+    productProperties["SEGMENT_UNITS"].split(",").each {
+        segmentUnitParameters += createBooleanParameter('', it)
+    }
+    def segmentUnitInputs = input(
+
+        id: 'userInput', message: 'Segment Level Units', parameters: segmentUnitParameters
+    )
+    def segmentUnits=[]
+    segmentUnitInputs?.findAll{ it.value }?.each {
+        segmentUnits += [ it.key.toString() ]
+    }
+    segmentUnits = segmentUnits.join(",")
+
+    // Solution Units
+    def solutionUnitParameters = []
+    productProperties["SOLUTION_UNITS"].split(",").each {
+        solutionUnitParameters += createBooleanParameter('', it)
+    }
+    def solutionUnitInputs = input(
+
+        id: 'userInput', message: 'Solution Level Units', parameters: solutionUnitParameters
+    )
+    def solutionUnits=[]
+    solutionUnitInputs?.findAll{ it.value }?.each {
+        solutionUnits += [ it.key.toString() ]
+    }
+    solutionUnits = solutionUnits.join(",")
+
+    // Application Units
+    def applicationUnitParameters = []
+    productProperties["APPLICATION_UNITS"].split(",").each {
+        applicationUnitParameters += createBooleanParameter( '', it)
+    }
+    def appliationUnitInputs = input(
+
+        id: 'userInput', message: 'Application Level Units', parameters: applicationUnitParameters
+    )
+    def applicationUnits=[]
+    appliationUnitInputs?.findAll{ it.value }?.each {
+        applicationUnits += [ it.key.toString() ]
+    }
+    applicationUnits = applicationUnits.join(",")
+    
+    // Call manage environment
+    cotTaskManageEnvironment(
+            propertiesFile: propertiesFile,
+            deploymentMode: basicParameters["DEPLOYMENT_MODE"],
+            environment: basicParameters["ENVIRONMENT"],
+            segment: basicParameters["SEGMENT"],
+            comment: basicParameters["COMMENT"],
+            treatRunIdAsSignificant: basicParameters["TREAT_RUN_ID_DIFFERENCES_AS_SIGNIFICANT"],
+            levelsList: levels,
+            segmentUnits: segmentUnits,
+            solutionUnits: solutionUnits,
+            applicationUnits: applicationUnits)
 }
--- vars/cotTaskManageEnvironment.groovy
+++ vars/cotTaskManageEnvironment.groovy
@@ -1,9 +1,30 @@
 #!/usr/bin/env groovy
 
-def call( String propertiesFile ) {
+def call( 
+        String propertiesFile, 
+        String deploymentMode, 
+        String environment, 
+        String segment, 
+        String comment, 
+        String treatRunIdAsSignificant,
+        String levelsList,
+        String segmentUnitsList,
+        String solutionUnitsList,
+        String applicationUnitsList ) {
+
+    def environmentVariables = [ 
+        "ENVIRONMENT=$environment", 
+        "DEPLOYMENT_MODE=$deploymentMode",
+        "SEGMENT=$segment",
+        "COMMENT=$comment",
+        "TREAT_RUN_ID_AS_SIGNIFICANT=$treatRunIdAsSignificant",
+        "LEVELS_LIST=$levelsList",
+        "SEGMENT_UNITS_LIST=$segmentUnitsList",
+        "SOLUTION_UNITS_LIST=$solutionUnitsList",
+        "APPLICATION_UNITS_LIST=$applicationUnitsList" ]
 
     def productProperties = readProperties interpolate: true, file: propertiesFile;
-    def environmentVariables = productProperties.collect {/$it.key=$it.value/ }
+    environmentVariables += productProperties.collect {/$it.key=$it.value/ }
 
     sh '''#!/bin/bash
     trap \'exit ${RESULT:-1}\' EXIT SIGHUP SIGINT SIGTERM
@@ -15,6 +36,8 @@ def call( String propertiesFile ) {
     environmentVariables += contextProperties.collect {/$it.key=$it.value/ }
 
     withEnv( environmentVariables ) {
+
+        sh 'env'
         
         sh '''#!/bin/bash
         trap \'exit ${RESULT:-1}\' EXIT SIGHUP SIGINT SIGTERM
