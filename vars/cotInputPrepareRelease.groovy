309f45fed27c0c158a17ed62593cce5c6f6581ea
roleyfoley <roleyfoley@gmail.com>
2019-03-07 16:09:26 +1100

    try again

--- vars/cotInputPrepareRelease.groovy
+++ vars/cotInputPrepareRelease.groovy
@@ -5,8 +5,10 @@ def call( String propertiesFile, String[]applicationDeploymentUnits ) {
     def defaultProperties = [GENERATION_DEBUG: '', AUTOMATION_DEBUG: '' ]
     productProperties = readProperties interpolate: true, file: propertiesFile, defaults: defaultProperties;
 
+    applicationDeploymentUnits = applicationDeploymentUnits.join('\n')
+
     def basicParameters = input message: 'Please Provide Parameters', ok: 'Start', parameters: [
-            text( name: 'DEPLOYMENT_UNITS' defaultValue: applicationDeploymentUnits.join('\n'), description: "Units to be updated as part of the release preparation. For those units where code references are to be updated, append the detail after the affected unit." )
+            text( name: 'DEPLOYMENT_UNITS' defaultValue: "${applicationDeploymentUnits}", description: "Units to be updated as part of the release preparation. For those units where code references are to be updated, append the detail after the affected unit." )
             string( name: 'RELEASE_IDENTIFIER' defaultValue: "${env.BUILD_NUMBER}", description: 'Identifier for the release. If not provided, the current build number will be used.' )
         ]
 
