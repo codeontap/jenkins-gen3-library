403f5854483068089a7d396f32ee81cabcab7e2c
roleyfoley <roleyfoley@gmail.com>
2019-03-07 15:42:26 +1100

    check env on construct tree

--- vars/cotTaskManageEnvironment.groovy
+++ vars/cotTaskManageEnvironment.groovy
@@ -16,6 +16,7 @@ def call( String propertiesFile ) {
     environmentVariables += contextProperties.collect {/$it.key=$it.value/ }
 
     withEnv( environmentVariables ) {
+        sh 'env'
         sh '''#!/bin/bash
         trap \'exit ${RESULT:-1}\' EXIT SIGHUP SIGINT SIGTERM
         ${AUTOMATION_DIR}/constructTree.sh
@@ -27,7 +28,6 @@ def call( String propertiesFile ) {
     environmentVariables += contextProperties.collect {/$it.key=$it.value/ }
 
     withEnv( environmentVariables ) {
-        sh 'env'
         sh '''#!/bin/bash
         trap \'exit ${RESULT:-1}\' EXIT SIGHUP SIGINT SIGTERM
         ${AUTOMATION_DIR}/manageEnvironment.sh
