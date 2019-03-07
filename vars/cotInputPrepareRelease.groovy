132c220f0d58fc0d9729d9e96060aceda2c651da
roleyfoley <roleyfoley@gmail.com>
2019-03-07 16:36:24 +1100

    try param map

--- vars/cotInputPrepareRelease.groovy
+++ vars/cotInputPrepareRelease.groovy
@@ -1,17 +1,17 @@
 #!/usr/bin/env groovy
 
-def call( String propertiesFile, List<String> applicationDeploymentUnits ) {
+def call( Map params = [:] ) {
 
+    def propertiesFile = params.get(propertiesFile, "")
+    def applicationDeploymentUnits = params.get(deploymentUnits, [ "" ] )
+    
     def defaultProperties = [GENERATION_DEBUG: '', AUTOMATION_DEBUG: '' ]
     productProperties = readProperties interpolate: true, file: propertiesFile, defaults: defaultProperties;
 
-    applicationDeploymentUnits = applicationDeploymentUnits.join('\n')
-
     def basicParameters = input message: 'Please Provide Parameters', ok: 'Start', parameters: [
-            text( name: 'DEPLOYMENT_UNITS' defaultValue: "${applicationDeploymentUnits}", description: "Units to be updated as part of the release preparation. For those units where code references are to be updated, append the detail after the affected unit." )
+            text( name: 'DEPLOYMENT_UNITS' defaultValue: applicationDeploymentUnits.join('\n'), description: "Units to be updated as part of the release preparation. For those units where code references are to be updated, append the detail after the affected unit." )
             string( name: 'RELEASE_IDENTIFIER' defaultValue: "${env.BUILD_NUMBER}", description: 'Identifier for the release. If not provided, the current build number will be used.' )
         ]
-
     env.DEPLOYMENT_UNITS = basicParameters["DEPLOYMENT_UNITS"]
     env.RELEASE_IDENTIFIER = basicParameters["RELEASE_IDENTIFIER"]    
 }
