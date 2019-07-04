ad16ed690beace9f0a134dd1fb29ca88281ab842
roleyfoley <roleyfoley@gmail.com>
2019-07-04 13:36:18 +1000

    add lookup for envvars

--- vars/cotTaskManageEnvironment.groovy
+++ vars/cotTaskManageEnvironment.groovy
@@ -1,10 +1,29 @@
 #!/usr/bin/env groovy
 
-def call() {
+def call( 
+        String      environment, 
+        String      segment, 
+        String      deploymentMode,
+        String      comment ,  
+        String[]    levelsList,
+        String[]    segmentUnits,
+        String[]    solutionUnits,
+        String[]    applicationUnits,
+        Boolean     runIdSignificant = false ) {
 
     skipDefaultCheckout(true)
 
     def environmentVariables = []
+
+    environmentVariables['ENVIRONMENT'] = environment ?: env.ENVIRONMENT
+    environmentVariables['SEGMENT'] = segment ?: env.SEGMENT
+    environmentVariables['deploymentMode'] = deploymentMode ?: env.DEPLOYMENT_MODE
+    environmentVariables['comment'] = deploymentMode ?: env.comment
+    environmentVariables['levelsList'] = levelsList ?: env.LEVELS_LIST
+    environmentVariables['segmentUnits'] = segmentUnits?join(',') ?: env.SEGMENT_UNITS
+    environmentVariables['solutionUnits'] = solutionUnits?join(',') ?: env.SOLUTION_UNITS
+    environmentVariables['applicationUnits'] = applicationUnits?join(',') ?: env.APPLICATION_UNITS
+
     def siteProperties = readProperties interpolate: true, file: cot.siteProperties();
     environmentVariables += siteProperties.collect {/$it.key=$it.value/ }
 
