ff3a519571097365163e7abede7a007ea1429ca6
roleyfoley <roleyfoley@gmail.com>
2019-07-04 15:35:25 +1000

    put environment in array

--- vars/cotTaskManageEnvironment.groovy
+++ vars/cotTaskManageEnvironment.groovy
@@ -4,22 +4,23 @@ def call( Map params = [:] ) {
 
     skipDefaultCheckout(true)
 
-    //levelsList          = (params.get('levelsList', [])).join(',')
-    //segmentUnits        = (params.get('segmentUnits', [])).join(',')
-    //solutionUnits       = (params.get('solutionUnits', [])).join(',')
-    //applicationUnits    = (params.get('applicationUnits', [])).join(',')
+    levelsList          = (params.get('levelsList', [])).join(',')
+    segmentUnits        = (params.get('segmentUnits', [])).join(',')
+    solutionUnits       = (params.get('solutionUnits', [])).join(',')
+    applicationUnits    = (params.get('applicationUnits', [])).join(',')
 
     def environmentVariables = []
 
     environmentVariables += [ "ENVIRONMENT=${params.get('environment', '')}" ]
-    //environmentVariables['SEGMENT']                                 = params.get('segment', '')
-    //environmentVariables['DEPLOYMENT_MODE']                         = params.get('deploymentMode', '') 
-    //environmentVariables['COMMENT']                                 = params.get('comment', '')
-    //environmentVariables['LEVELS_LIST']                             = levelsList
-    //environmentVariables['SEGMENT_UNITS_LIST']                      = segmentUnits
-    //environmentVariables['SOLUTION_UNITS_LIST']                     = solutionUnits
-    //environmentVariables['APPLICATION_UNITS_LIST']                  = applicationUnits
-    //environmentVariables['TREAT_RUN_ID_DIFFERENCES_AS_SIGNIFICANT'] = params.get('runIdSignificant', '')
+    environmentVariables += [ "SEGMENT=${params.get('segment', '')}" ]
+    environmentVariables += [ "DEPLOYMENT_MODE=${params.get('deploymentMode', '')}" ]
+    environmentVariables += [ "COMMENT=${params.get('comment', '')}" ]
+    environmentVariables += [ "TREAT_RUN_ID_DIFFERENCES_AS_SIGNIFICANT=${params.get('runIdSignificant', '')}" ]
+
+    environmentVariables += [ "LEVELS_LIST=${levelsList}" ]
+    environmentVariables += [ "LEVELS_LIST=${segmentUnits}" ]
+    environmentVariables += [ "LEVELS_LIST=${solutionUnits}" ]
+    environmentVariables += [ "LEVELS_LIST=${applicationUnits}" ]
 
     echo "${environmentVariables}"
 
